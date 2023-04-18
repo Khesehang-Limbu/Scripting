dbcf48ac214580e4817e4f633f8db48b240c975c
Khesehang Yonghang <Khesehang Yonghang>
2023-04-18 09:19:18 +0545

    Learn Array and its methods

---
+++ Day8/index.js
@@ -0,0 +1,41 @@
+const data = ["Banana", "Mo:Mo", "Pizza"];
+data.push("Dal-Bhat");
+data.unshift("Egg Roll");//Adds element at first index
+data.shift()//Remove first element
+data.pop()//Removes last element
+
+//Unshift puts an item at the beginning of the array
+console.log(data);
+
+const dataDiv = document.getElementById("data");
+
+
+if (data.length >= 6){    
+    data.forEach(item => {
+        const eachItem = document.createElement("div");
+        eachItem.innerHTML = item + "<br />";
+        dataDiv.appendChild(eachItem);
+    });
+}else {
+    document.write("<h2>More Elements<h2/>");
+}
+
+let userData = prompt("Enter your hobby : ");
+let userVerification = confirm("Do you want to proceed?");
+
+if (userVerification === true){
+
+switch (userData) {
+    case "football":
+        document.write(`<em>I love playing <strong>${userData}</strong></em>`);
+        break;
+    case "volleyball":
+    case "cricket":
+    case "snooker":
+        document.write(`My hobby is playing ${userData}`);
+        break;
+    default:
+        document.write("I don't know any sports");
+        break;
+}
+}
